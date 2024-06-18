5eeca7af8f756b5b9f716a03bc87b34ce58f1ca0
Chandu-git90 <chandra.j1190@gmail.com>
2024-06-18 21:32:40 +0530

    initial commit

---
+++ http-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash/
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl start httpd
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cenable httpd
---
+++ if.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+x=10
+if [ $x -ge 10 ]
+echo " statement is true: "
+else
+echo "statement is false"
+fi
