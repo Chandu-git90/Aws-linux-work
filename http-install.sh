ae5a9417a38fd8a02a5be5dd6cc5a596b6cc8181
Chandu-git90 <chandra.j1190@gmail.com>
2024-06-18 21:49:12 +0530

    updated restart command

--- http-install.sh
+++ http-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash/
 sudo yum install httpd -y
 sudo systemctl start httpd
-sudo systemctl start httpd
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cenable httpd
+sudo systemctl enable httpd
+sudo systemctl restart httpd
+
